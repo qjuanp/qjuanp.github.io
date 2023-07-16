56b0dc0c205ea3de7f1bd74e2835beb58dd3632f
Juan P <code@qjuanp.dev>
2023-07-16 20:47:03 +0100

    Added sass support, added css to base html

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
+const eleventySass = require("@grimlink/eleventy-plugin-sass");
+const sass = require('sass');
 const absoluteUrl = require("./src/filters/abasoluteUrl");
 
 module.exports = config => {
-
+  // Custom filters
   config.addNunjucksFilter("absoluteUrl", absoluteUrl);
+  
+  // sass
+  config.addPlugin(eleventySass, { 
+    sass,  
+    outputPath: 'css'
+  });
+
 
   return {
     dir: {
